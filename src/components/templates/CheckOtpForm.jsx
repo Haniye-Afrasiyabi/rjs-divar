c9d062850ea74ccdec085f85bd08a0dfdc05352a
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-11-03 02:01:36 +0330

    Step two form to get OTP via SMS

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,5 +1,24 @@
-function CheckOtpForm() {
-  return <div>CheckOtpForm</div>;
+function CheckOtpForm({ mobile, code, setCode, setStep }) {
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log({ mobile, code });
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <p>تایید کد اس ام اس شده</p>
+      <span>کد پیامک شده به شماره {mobile} را وارد کنید.</span>
+      <label htmlFor="input">کد تایید را وارد کنید.</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="کد تایید"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button type="submit">ورود</button>
+      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+    </form>
+  );
 }
 
 export default CheckOtpForm;
