d1b41ebc950a99e4650479bc291df0dc3f603d9e
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-11-03 00:37:22 +0330

    Send mobile number to backend

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -1,7 +1,15 @@
+import { sendOtp } from "../../services/auth";
+
 function SendOtpForm({ mobile, setMobile, setStep }) {
-  const submitHandler = (event) => {
+  const submitHandler = async (event) => {
     event.preventDefault();
-    console.log(event);
+    console.log(mobile);
+
+    if (mobile.length !== 11) return;
+    const { response, error } = await sendOtp(mobile);
+    if (response) setStep(2);
+    if (error) console.log(error.response.data.message);
+    console.log({ response, error });
   };
   return (
     <form onSubmit={submitHandler}>
