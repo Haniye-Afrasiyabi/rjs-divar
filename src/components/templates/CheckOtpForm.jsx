96c0192d29012f3b61ec782147284f11900fc691
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-11-16 02:23:30 +0330

    Adding CSS to Forms

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -22,9 +22,14 @@ function CheckOtpForm({ mobile, code, setCode, setStep }) {
     if (error) console.log(error.response.data.message);
   };
   return (
-    <form onSubmit={submitHandler}>
-      <p>تایید کد اس ام اس شده</p>
-      <span>کد پیامک شده به شماره {mobile} را وارد کنید.</span>
+    <form
+      onSubmit={submitHandler}
+      className="max-w-md m-auto flex flex-col mt-24 border border-gray-300 rounded-md p-7"
+    >
+      <p className="text-base font-normal mb-5">تایید کد اس ام اس شده</p>
+      <span className="text-gray text-sm mb-5">
+        کد پیامک شده به شماره {mobile} را وارد کنید.
+      </span>
       <label htmlFor="input">کد تایید را وارد کنید.</label>
       <input
         type="text"
@@ -32,9 +37,20 @@ function CheckOtpForm({ mobile, code, setCode, setStep }) {
         placeholder="کد تایید"
         value={code}
         onChange={(e) => setCode(e.target.value)}
+        className="mt-2.5 mb-5 p-1 border border-gray-300 rounded-md"
       />
-      <button type="submit">ورود</button>
-      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+      <button
+        type="submit"
+        className="w-16 py-1 px-2 border-none bg-[#a62626] text-white rounded cursor-pointer"
+      >
+        ورود
+      </button>
+      <button
+        onClick={() => setStep(1)}
+        className="bg-white border border-[#a62626] rounded w-36 mt-7 text-[#a62626]"
+      >
+        تغییر شماره موبایل
+      </button>
     </form>
   );
 }
--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -12,10 +12,13 @@ function SendOtpForm({ mobile, setMobile, setStep }) {
     console.log({ response, error });
   };
   return (
-    <form onSubmit={submitHandler}>
-      <p>ورود به حساب کاربری</p>
-      <span>
-        برای استفاده از امکنات دیوار، لطفا شماره موبایل خود را وارد کنید. کد
+    <form
+      onSubmit={submitHandler}
+      className="max-w-md m-auto flex flex-col mt-24 border border-gray-300 rounded-md p-7"
+    >
+      <p className="text-base font-normal mb-5">ورود به حساب کاربری</p>
+      <span className="text-gray text-sm mb-5">
+        برای استفاده از امکانات دیوار، لطفا شماره موبایل خود را وارد کنید. کد
         تایید به این شماره پیامک خواهد شد.
       </span>
       <label htmlFor="input">شماره موبایل خود را وراد کنید.</label>
@@ -25,8 +28,14 @@ function SendOtpForm({ mobile, setMobile, setStep }) {
         placeholder="شماره موبایل"
         value={mobile}
         onChange={(e) => setMobile(e.target.value)}
+        className="mt-2.5 mb-5 p-1 border border-gray-300 rounded-md"
       />
-      <button type="submit">ارسال کد تایید</button>
+      <button
+        type="submit"
+        className="w-28 py-1 px-2 border-none bg-[#a62626] text-white rounded cursor-pointer"
+      >
+        ارسال کد تایید
+      </button>
     </form>
   );
 }
