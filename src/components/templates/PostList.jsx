a559584769200c0a96af7f570abd22f2676704e1
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-11-24 21:45:46 +0330

    Showing user ads in the user panel

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,15 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
 import { getPosts } from "services/user";
+import Loader from "../modules/Loader";
 
 function PostList() {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
   const queryKey = ["my-post-list"];
   const queryFn = getPosts;
-  const { data } = useQuery({ queryKey, queryFn });
+  const { data, isPending } = useQuery({ queryKey, queryFn });
   console.log(data);
 
   return (
     <div>
-      <h1>PostList</h1>
+      {isPending ? (
+        <Loader />
+      ) : (
+        <>
+          <h3>آگهی‌های شما</h3>
+          {data.data.posts.map((post) => (
+            <div key={post._id}>
+              <img src={`${baseUrl}${post.images[0]}`} />
+              <div>
+                <p>{post.title}</p>
+                <span>{post.content}</span>
+              </div>
+              <div>
+                <p>{post.createdAt}</p>
+                <span>{post.amount} تومان</span>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
