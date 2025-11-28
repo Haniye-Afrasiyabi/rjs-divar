7287218932aa0fc997a36d114a99a99545c77152
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-11-28 13:56:08 +0330

    Adding CSS to the ad list

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -8,7 +8,6 @@ function PostList() {
   const queryKey = ["my-post-list"];
   const queryFn = getPosts;
   const { data, isPending } = useQuery({ queryKey, queryFn });
-  console.log(data);
 
   return (
     <div>
@@ -16,17 +15,31 @@ function PostList() {
         <Loader />
       ) : (
         <>
-          <h3>آگهی‌های شما</h3>
+          <h3 className="mt-14 mb-8 border-b-4 border-[#a62626] w-fit pb-1">
+            آگهی‌های شما
+          </h3>
           {data.data.posts.map((post) => (
-            <div key={post._id}>
-              <img src={`${baseUrl}${post.images[0]}`} />
-              <div>
-                <p>{post.options.title}</p>
-                <span>{post.options.content}</span>
+            <div
+              key={post._id}
+              className="flex items-center border-2 border-[#eaeaea] rounded my-2.5 p-1"
+            >
+              <img
+                src={`${baseUrl}${post.images[0]}`}
+                className="w-24 h-20 rounded-sm ml-7 "
+              />
+              <div className="w-full">
+                <p className="text-base">{post.options.title}</p>
+                <span className="text-sm text-gray-500">
+                  {post.options.content}
+                </span>
               </div>
-              <div>
-                <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
-                <span>{sp(post.amount)} تومان</span>
+              <div className="w-36 text-center">
+                <p className="text-base">
+                  {new Date(post.createdAt).toLocaleDateString("fa-IR")}
+                </p>
+                <span className="text-sm text-gray-500">
+                  {sp(post.amount)} تومان
+                </span>
               </div>
             </div>
           ))}
