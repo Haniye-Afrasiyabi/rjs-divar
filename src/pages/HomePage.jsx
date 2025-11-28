0e1cf241257d1f85f3b1ae2f629e42305a6f617d
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-11-28 14:11:58 +0330

    Home page of the site

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
+import Main from "components/templates/Main";
+import Sidebar from "components/templates/Sidebar";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Main />
+    </div>
+  );
 }
 
 export default HomePage;
