9b72d4fb56c79d080658544dd3200eba192e497b
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-12-20 03:20:02 +0330

    Show list of all ads

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,6 +1,24 @@
+import { sp } from "src/utils/numbers";
+
 function Main({ posts }) {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
   console.log(posts);
-  return <div>Main</div>;
+  return (
+    <div>
+      {posts.data.posts.map((post) => (
+        <div key={post._id}>
+          <div>
+            <p>{post.title} </p>
+            <div>
+              <p>{sp(post.amount)}</p>
+              <span>{post.city}</span>
+            </div>
+          </div>
+          <img src={`${baseUrl}${post.images[0]}`} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Main;
