903642f03c7619580f3aeceaee93e601afbd4410
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-12-19 19:56:00 +0330

    feat: implement sidebar with category structure and style: add styling for sidebar

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,5 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCategory } from "src/services/admin";
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const queryKey = ["get-categories"];
+  const queryFn = getCategory;
+  const { data } = useQuery({ queryKey, queryFn });
+
+  return (
+    <div className="mt-7 w-[200px]">
+      <h4>دسته‌ها</h4>
+      <ul>
+        {data?.data.map((category) => (
+          <li key={category._id} className="flex my-5 cursor-pointer">
+            <img src={`${category.icon}.svg`} />
+            <p className="font-light mr-2.5 text-gray-500">{category.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
