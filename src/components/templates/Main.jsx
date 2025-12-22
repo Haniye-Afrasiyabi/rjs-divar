e958dd2b020241d378b5c134bffa6eb3ace6084d
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-12-23 01:57:59 +0330

    Adding CSS to the home page

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -5,17 +5,23 @@ function Main({ posts }) {
 
   console.log(posts);
   return (
-    <div>
+    <div className="flex flex-wrap justify-between mt-5 w-[calc(100%-200px)]">
       {posts.data.posts.map((post) => (
-        <div key={post._id}>
-          <div>
-            <p>{post.title} </p>
-            <div>
+        <div
+          key={post._id}
+          className="w-[330px] flex justify-between border border-[#eaeaea] m-2.5 p-4 rounded-[5px]"
+        >
+          <div className="flex flex-col justify-between">
+            <p className="text-sm">{post.title} </p>
+            <div className="text-gray-500 text-base">
               <p>{sp(post.amount)}</p>
               <span>{post.city}</span>
             </div>
           </div>
-          <img src={`${baseUrl}${post.images[0]}`} />
+          <img
+            src={`${baseUrl}${post.images[0]}`}
+            className="w-[150px] h-[130px] rounded-[3px]"
+          />
         </div>
       ))}
     </div>
